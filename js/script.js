70431adc48d33c63032dc57b146d78590aec593f
simone-danzeri <s.danzeri@icloud.com>
2024-04-02 13:46:15 +0200

    feat: creato array di oggetti con le task iniziali

--- js/script.js
+++ js/script.js
@@ -15,7 +15,28 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            tasks: [
+                {
+                    text: 'Pulire la casa',
+                    done: false
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                },
+                {
+                    text: 'Buttare la spazzatura',
+                    done: false
+                },
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Cambiare la lampadina',
+                    done: false
+                },
+            ]
         };
     },
     methods : {
