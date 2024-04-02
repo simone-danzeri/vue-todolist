47af394d7cd235b6baf0f540075bc64ceabb2556
simone-danzeri <s.danzeri@icloud.com>
2024-04-02 13:43:34 +0200

    fet: creato html e js di base

--- js/script.js
+++ js/script.js
@@ -10,3 +10,15 @@ MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. */
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            
+        };
+    },
+    methods : {
+
+    }
+}).mount('#app');
